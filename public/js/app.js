181f7b5b33c3befef8f8a6c943d744de4e780dff
hamzashiva <hamzaghaydi01@gmail.com>
2025-05-08 01:49:52 +0100

    menu

--- public/js/app.js
+++ public/js/app.js
@@ -2,14 +2,73 @@ const burger = document.getElementById('menu')
 const navigation = document.getElementById('navigation')
 const close = document.getElementById('close')
 
-burger.addEventListener('click' , ()=>{
+burger.addEventListener('click', () => {
     navigation.style.display = "flex"
     document.body.style.background = "#0c0b09dc"
     document.body.style.overflow = "hidden"
 })
 
-close.addEventListener('click' , () => {
+close.addEventListener('click', () => {
     navigation.style.display = "none"
     document.body.style.background = "none"
 })
 
+
+// menu
+
+const allTabsBtn = document.getElementById('all-tabs')
+const startersBtn = document.getElementById('starters')
+const saladesBtn = document.getElementById('salades')
+const specialtyBtn = document.getElementById('specialty')
+
+
+const allPlats = document.querySelectorAll('.plats')
+
+
+allPlats.forEach(plat => {
+  plat.style.display = 'flex'; 
+})
+
+
+allTabsBtn.addEventListener('click', () => {
+  allPlats.forEach(plat => {
+    plat.style.display = 'flex';
+  })
+})
+
+
+startersBtn.addEventListener('click', () => {
+  allPlats.forEach(plat => {
+    if (plat.classList.contains('starters-plat')) {
+      plat.style.display = 'flex';
+    } else {
+      plat.style.display = 'none';
+    }
+  })
+})
+
+
+saladesBtn.addEventListener('click', () => {
+  allPlats.forEach(plat => {
+    if (plat.classList.contains('salades')) {
+      plat.style.display = 'flex';
+    } else {
+      plat.style.display = 'none';
+    }
+  })
+})
+
+
+specialtyBtn.addEventListener('click', () => {
+  allPlats.forEach(plat => {
+    if (plat.classList.contains('specialty')) {
+      plat.style.display = 'flex';
+    } else {
+      plat.style.display = 'none';
+    }
+  })
+})
+
+
+
+  
